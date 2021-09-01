0eeb1dbc59cd9b56eddcf6a5f70c6222bdb924b6
almidds <almidds@hotmail.com>
2021-09-01 19:47:21 +0100

    Added decimal button & bug fixes

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const display = document.querySelector(".output");
 //finds all numbers
 const numbers = document.querySelectorAll(".number");
 numbers.forEach(number => {
-    number.addEventListener("mousedown", appendNumber);
+    number.addEventListener("click", appendNumber);
 });
 
 //finds all operators
 const operators = document.querySelectorAll(".operation");
 operators.forEach(operator =>{
-    operator.addEventListener("mousedown", setOperation);
+    operator.addEventListener("click", setOperation);
 });
 
 //finds the equals button
@@ -25,10 +25,24 @@ clear.addEventListener("click", clearEntry);
 const deleteButton = document.querySelector(".delete");
 deleteButton.addEventListener("click", deleteEntry);
 
+//finds the decimal button
+const decimalButton = document.querySelector(".dot");
+decimalButton.addEventListener("click", addDecimal);
+
 let tempNumber = "0";
 let firstNumber = 0;
 let secondNumber = 0;
 let currentOperation = "";
+
+const firstNumberDisplay = document.querySelector("#firstNumber");
+const secondNumberDisplay = document.querySelector("#secondNumber");
+
+// Good for bug fixes
+// setInterval(function(){
+//     firstNumberDisplay.innerText = `first number: ${firstNumber}`;
+//     secondNumberDisplay.innerText = `second number: ${secondNumber}`;
+
+// }, 100);
 
 //performs calculation
 function operate(a, b, operation){
@@ -45,8 +59,30 @@ function operate(a, b, operation){
 }
 
 //updates the screen with passed through number
-function updateDisplay(number){   
-    display.innerText = number; 
+function updateDisplay(number){
+    number = String(number);
+    if((number.length >= 12 && number.includes(".")) || number.length == null){
+        alert("got here");
+        alert(number.split(".")[0]);
+        if(number.split(".")[0].length >= 10){
+            display.innerText = "Too big :(";
+            firstNumber = 0;
+            secondNumber = 0;
+            tempNumber = 0;
+            currentOperation = "";
+        }else{
+            display.innerText = number.substring(0,10);
+        }
+    }else if(number.length >= 12){
+        display.innerText = "Too big :(";
+        firstNumber = 0;
+        secondNumber = 0;
+        tempNumber = 0;
+        currentOperation = "";
+    }else{
+        display.innerText = number; 
+    }
+    
 }
 
 //appends pressed number to temp number
@@ -54,7 +90,10 @@ function appendNumber(e){
     if(tempNumber == "0"){
         tempNumber = e.target.dataset.number;
     }else{
-        tempNumber += e.target.dataset.number;
+        if(tempNumber.length != 11){
+            tempNumber += e.target.dataset.number;
+        }
+        
     }
     updateDisplay(tempNumber);
 }
@@ -62,29 +101,47 @@ function appendNumber(e){
 //updates the current function
 function setOperation(e){
     if(currentOperation == ""){
-        currentOperation = e.target.dataset.operation;
-        firstNumber = parseFloat(tempNumber);
-        tempNumber = "";
-    }else{
-        firstNumber = operate(firstNumber, parseFloat(tempNumber), currentOperation);
-        currentOperation = e.target.dataset.operation;
-        updateDisplay(firstNumber);
-        tempNumber = "";
-    }
+        if(firstNumber == 0){
+            currentOperation = e.target.dataset.operation;
+            firstNumber = parseFloat(tempNumber);
+            tempNumber = "";
+        }else{
+            currentOperation = e.target.dataset.operation;
+            tempNumber = "";
+        }
 
+    }else{
+        if(tempNumber != ""){
+            firstNumber = operate(firstNumber, parseFloat(tempNumber), currentOperation);
+            currentOperation = e.target.dataset.operation;
+            updateDisplay(firstNumber);
+            tempNumber = "";
+        }
+    }
 }
 
 //evaluates the function when the equals sign is pressed
 function eval(){
-    if(firstNumber != 0){
-        secondNumber = parseFloat(tempNumber);
-        let result = operate(firstNumber, secondNumber, currentOperation);
-        updateDisplay(result);
-        return(result);
+    if(parseFloat(tempNumber) == 0 && currentOperation == "divide"){
+        updateDisplay("no thanks");
+        firstNumber = 0;
+        secondNumber = 0;
+        tempNumber = 0;
+        currentOperation = "";
+    }else{
+        if(firstNumber != 0){
+            secondNumber = parseFloat(tempNumber);
+            let result = operate(firstNumber, secondNumber, currentOperation);
+            firstNumber = parseFloat(result);
+            secondNumber = 0;
+            tempNumber = "";
+            currentOperation = "";
+            updateDisplay(result);
+        }
     }
-    return;
 }
 
+//Erases everything stored
 function clearEntry(){
     firstNumber = 0;
     secondNumber = 0;
@@ -93,8 +150,17 @@ function clearEntry(){
     updateDisplay(tempNumber);
 }
 
-function deleteEntry(e){
+//Acts as a backspace when typing a number out
+function deleteEntry(){
     tempNumber = tempNumber.substring(0,tempNumber.length-1);
+    updateDisplay(tempNumber);
+}
+
+//Adds decimal place to number
+function addDecimal(){
+    if(tempNumber.length != 11){
+        tempNumber += ".";
+    }
     updateDisplay(tempNumber);
 }
 
@@ -102,3 +168,4 @@ function deleteEntry(e){
 window.onload = () =>{
     updateDisplay(tempNumber);
 }
+
